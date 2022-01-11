7dd18d57ab7ce9c2bfa7ccccf9be46d3ecb771af
Nasr-Edine DRAI <d.nasredine@gmail.com>
2022-01-11 14:15:02 +0100

    add pagination

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from .serializers import UserSerializer, GroupSerializer
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ 
+    API endpoint that allows users to be viewed or edited
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """ 
+    API endpoint that allows groups to be viewed or edited
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
